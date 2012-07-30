fad802c0552ace87ffc3d45e91dfa9fa96113e4d
liuhong <myoldman@163.com>
2012-07-30 09:59:49 +0800

    删除rtsp相关代码

--- media/src/h264/h264decoder.cpp
+++ media/src/h264/h264decoder.cpp
@@ -4,7 +4,45 @@
 #include <netinet/in.h>
 #include "log.h"
 #include "h264decoder.h"
-#include "Base64.hh"
+
+char* base64Encode(char const* origSigned, unsigned origLength) 
+{  
+	unsigned char const* orig = (unsigned char const*)origSigned; 
+	// in case any input bytes have the MSB set 
+	if (orig == NULL) return NULL;  
+	unsigned const numOrig24BitValues = origLength/3;  
+	Boolean havePadding = origLength > numOrig24BitValues*3;  
+	Boolean havePadding2 = origLength == numOrig24BitValues*3 + 2;  
+	unsigned const numResultBytes = 4*(numOrig24BitValues + havePadding); 
+	char* result = new char[numResultBytes+1]; 
+	// allow for trailing '\0'  
+	// Map each full group of 3 input bytes into 4 output base-64 characters:  
+	unsigned i;  
+	for (i = 0; i < numOrig24BitValues; ++i) 
+	{    
+		result[4*i+0] = base64Char[(orig[3*i]>>2)&0x3F];    
+		result[4*i+1] = base64Char[(((orig[3*i]&0x3)<<4) | (orig[3*i+1]>>4))&0x3F];    
+		result[4*i+2] = base64Char[((orig[3*i+1]<<2) | (orig[3*i+2]>>6))&0x3F];    
+		result[4*i+3] = base64Char[orig[3*i+2]&0x3F];  
+	}  
+
+	// Now, take padding into account.  (Note: i == numOrig24BitValues)  
+	if (havePadding) 
+	{    
+		result[4*i+0] = base64Char[(orig[3*i]>>2)&0x3F];    
+		if (havePadding2) 
+		{      
+			result[4*i+1] = base64Char[(((orig[3*i]&0x3)<<4) | (orig[3*i+1]>>4))&0x3F];
+			result[4*i+2] = base64Char[(orig[3*i+1]<<2)&0x3F];    
+		} else {     
+			result[4*i+1] = base64Char[((orig[3*i]&0x3)<<4)&0x3F];      
+			result[4*i+2] = '=';    
+		}    
+		result[4*i+3] = '=';  
+	}  
+	result[numResultBytes] = '\0';  
+	return result;
+}
 
 //H264Decoder
 // 	Decodificador H264
